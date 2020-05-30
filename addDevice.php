47f925281cbc7ee84fdcdea2e378c8e7427710ed
bharathreddypanyala <bharathintern@gmail.com>
2020-05-30 23:55:21 +0200

    add device file to add the device and store in database

--- addDevice.php
+++ addDevice.php
@@ -1,1 +1,25 @@
+<?php
+
+include('config.php');
+
+$ip = $_GET['ip'];
+$port = $_GET['port'];
+$community = $_GET['community'];
+$version = $_GET['version'];
+
+if(empty($ip) || empty($port) || empty($community) || empty($version)) {
+    echo "provide valid input" ;   
+}
+
+else {
+
+    $db->exec("INSERT INTO info (IP,PORT,COMMUNITY,VERSION) VALUES ('$ip','$port','$community','$version')");
+        echo "\n";
+        echo "OK added successfully";
+    
+    }
+$db->close();
+
+?>
+
 
