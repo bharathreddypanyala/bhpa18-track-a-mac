0bd607e20ab43c20fb60ef8e908ff21f39dfbc99
bharatreddypanyala <bhpa18@student.bth.se>
2020-11-28 17:35:18 +0100

    Change list

--- list.php
+++ list.php
@@ -2,7 +2,7 @@
 
 include('config.php');
 
-$result = $db->query('SELECT * FROM List');
+$result = $db->query('SELECT * FROM results');
 
 while($row = $result->fetchArray(SQLITE3_ASSOC) ) {
 	echo  $row['IP']. "|" .$row['VLANs']. "|" .$row['PORT']. "|" .$row['MACS']. "\n";
