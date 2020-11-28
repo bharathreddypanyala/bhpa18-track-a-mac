5b2fc47c37de126c3e6b6b806d123d56d339f75e
bharatreddypanyala <bhpa18@student.bth.se>
2020-11-28 17:35:02 +0100

    Change config

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-class mydb extends SQLite3 {
+class track_a_mac_db extends SQLite3 {
       function __construct() {
          $this->open('track-a-mac.db');
       }
 }
-$db = new mydb();
+$db = new track_a_mac_db();
 
 
 $result = $db->exec('CREATE TABLE IF NOT EXISTS results(IP varchar not null, VLANs varchar not null, PORT varchar, MACS varchar)');
@@ -12,9 +12,11 @@ if(!$result){
    echo $db->lastErrorMsg(); 
 }
 
+
 $result = $db->exec('CREATE TABLE IF NOT EXISTS networkdevices(IP varchar not null,PORT varchar not null,COMMUNITY string not null ,VERSION varchar not null, FIRST_PROBE varchar, LATEST_PROBE varchar null, FAILED_ATTEMPTS int default 0 not null)');
-   if(!$result){
+if(!$result)
+{
       echo $db->lastErrorMsg();
-   }
+}
 
 ?>
